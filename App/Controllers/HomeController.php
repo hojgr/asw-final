32e1c3d7f1cd6af901ec439d071b321b5ce538a7
Michal Hojgr <michal.hojgr@gmail.com>
2015-05-14 20:49:53 +0200

    app: add POST route for development

--- App/Controllers/HomeController.php
+++ App/Controllers/HomeController.php
@@ -6,14 +6,18 @@ namespace App\Controllers;
 
 class HomeController {
 	public function index() {
-		echo "<h1>waaat</h1>";
+		echo "<h1>waaat</h1>\n";
 	}
 
 	public function foo() {
-		echo "<h1>foo!!</h1>";
+		echo "<h1>foo!!(get) " . $_SERVER['REQUEST_METHOD'] . "</h1>\n";
+	}
+
+	public function fooPost() {
+		echo "FooPost!\n";
 	}
 
 	public function bar($in) {
-		echo "<h1>Bar $in!!!</h1>";
+		echo "<h1>Bar $in!!!</h1>\n";
 	}
 }
