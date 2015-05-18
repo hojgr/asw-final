ebe9288e9e65d140b35c85be817988e014b6f398
Michal Hojgr <michal.hojgr@gmail.com>
2015-05-18 14:23:35 +0200

    structure: make / be index, remove other routes

--- App/Controllers/HomeController.php
+++ App/Controllers/HomeController.php
@@ -9,18 +9,6 @@ use App\Core\Response\ViewResponse;
 
 class HomeController {
 	public function index() {
-		return new ViewResponse("<h1>what</h1>\n");
-	}
-
-	public function foo() {
-		return new ViewResponse("Home/foo", ["test" => "one two three four"]);
-	}
-
-	public function fooPost() {
-		return new TextResponse("Text response!");
-	}
-
-	public function bar($in) {
-		return new ViewResponse("<h1>Bar $in!!!</h1>\n");
+		return new ViewResponse("Home/index");
 	}
 }
--- App/Templates/Home/fooPost.php
+++
@@ -1,1 +0,0 @@
-Post layout! WUT WUT
